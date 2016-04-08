c256bcb37e916fb26311a1cba6fb999ec0c0324f
fabs <faebeee@gmail.com>
2016-04-08 22:49:38 +0200

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,16 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 
-gulp.task('default', function() {
+gulp.task('complex', function() {
   return gulp.src([
-    './lib/*.js',
-    './src/**/*.js',
+      './lib/dat.gui.js',
+      './src/**/*.js',
     ])
     .pipe(concat('complex-debug.js'))
     .pipe(gulp.dest('dist/'));
+});
+
+
+gulp.task('default', function() {
+    gulp.start('complex');
 });
